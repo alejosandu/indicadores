137e371090c7d370b30d6b6a7c85f55b15d3c07b
Alejandro Sanchez <alejosandu@gmail.com>
2020-04-24 18:09:19 -0500

    primer captura de campo para creación de indicador

--- front/src/views/CrearIndicador/CrearIndicador.js
+++ front/src/views/CrearIndicador/CrearIndicador.js
@@ -1,20 +1,55 @@
-import React from 'react';
-import { Container , Card , CardHeader , CardContent , CardFooter , Button } from 'components'
+import React, { useState } from 'react';
+import {
+   Container,
+   Card,
+   CardHeader,
+   CardContent,
+   CardFooter,
+   Button,
+   GridContainer,
+   GridItem,
+   InputTextField as Input
+} from 'components'
 
 const CrearIndicador = (props) => {
-    return (
-        <Container lg={12} md={12} xl={12} xs={12} >
-            <Card>
-                <CardHeader title="Hola" />
-                <CardContent>
-                    <h1>Esto es un mensaje de prueba</h1>
-                </CardContent>
-                <CardFooter>
-                    <Button color="primary" variant="contained" >Guardar</Button>
-                </CardFooter>
-            </Card>
-        </Container>
-    )
+   const [state, setState] = useState({})
+
+   const onChange = (e) => {
+      setState({
+         ...state,
+         [e.target.name]: e.target.value
+      });
+   }
+
+   return (
+      <Container lg={12} md={12} xl={12} xs={12} >
+         <Card>
+            <CardHeader
+               title="Crear nuevo indicador"
+            />
+            <CardContent>
+               <form autoComplete="off" noValidate >
+                  <GridContainer>
+                     <GridItem>
+                        <Input
+                           required
+                           displayName="Nombre de indicador"
+                           fieldName="indicadorNombre"
+                           value={state.indicadorNombre}
+                           inputEvents={{
+                              onChange
+                           }}
+                        />
+                     </GridItem>
+                  </GridContainer>
+               </form>
+            </CardContent>
+            <CardFooter>
+               <Button color="primary" variant="contained" >Guardar</Button>
+            </CardFooter>
+         </Card>
+      </Container>
+   )
 }
 
 export default CrearIndicador
