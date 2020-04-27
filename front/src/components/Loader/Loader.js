05edcfd654406c0ec3fd97f1c2fe504d2f127e13
Alejandro Sanchez <alejosandu@gmail.com>
2020-04-27 17:28:22 -0500

    ajuste mejora en loader

--- front/src/components/Loader/Loader.js
+++ front/src/components/Loader/Loader.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import Collapse from '@material-ui/core/Collapse';
+import Fade from '@material-ui/core/Fade';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from '@material-ui/core/Grid'
 
@@ -38,9 +39,38 @@ const Loader = (props) => {
         minWidth: "100%"
     }
 
+    if( !props.fade ) {
+        return ReactDOM.createPortal(
+            <div>
+                <Collapse
+                    style={portalStyle}
+                    in={loading}
+                    timeout={
+                        {
+                            enter: enterTime,
+                            exit: exitTime
+                        }
+                    }
+                >
+                    <Grid
+                        container
+                        direction="column"
+                        justify="center"
+                        alignItems="center"
+                        style={{ minHeight: '100vh' }}
+                    >
+                        <Grid item>
+                            <CircularProgress {...circularProps} />
+                        </Grid>
+                    </Grid>
+                </Collapse>
+            </div>, document.getElementById("loader-root")
+        )
+    }
+
     return ReactDOM.createPortal(
         <div>
-            <Collapse
+            <Fade
                 style={portalStyle}
                 in={loading}
                 timeout={
@@ -61,7 +91,7 @@ const Loader = (props) => {
                         <CircularProgress {...circularProps} />
                     </Grid>
                 </Grid>
-            </Collapse>
+            </Fade>
         </div>, document.getElementById("loader-root")
     )
 }
